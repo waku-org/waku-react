7717664bff6ff0a8c61fdcb1547ca38b4a7b264b
Sasha <oleksandr@status.im>
2023-02-16 16:01:57 +0100

    create useContentPair hook

---
+++ src/useContentPair.ts
@@ -0,0 +1,38 @@
+import {
+    useState,
+    useEffect,
+} from "react";
+import {
+    createDecoder,
+    createEncoder,
+} from "@waku/core";
+import type {
+    Encoder,
+    Decoder
+} from "@waku/core/dist/lib/message/version_0";
+
+type ContentPair = {
+    encoder: null | Encoder,
+    decoder: null | Decoder,
+};
+
+/**
+ * Creates Encoder / Decoder pair for a given contentTopic.
+ * @param {string} contentTopic - topic to orient to
+ * @param {boolean} ephemeral - optional, makes messages ephemeral
+ * @returns {Object} Encoder / Decoder pair
+ */
+export const useContentPair = (contentTopic: string, ephemeral?: boolean): ContentPair => {
+    const [encoder, setEncoder] = useState<null | Encoder>(null);
+    const [decoder, setDecoder] = useState<null | Decoder>(null);
+
+    useEffect(() => {
+        setEncoder(createEncoder(contentTopic, ephemeral));
+        setDecoder(createDecoder(contentTopic));
+    }, [contentTopic, ephemeral]);
+
+    return {
+        encoder,
+        decoder,
+    };
+};
